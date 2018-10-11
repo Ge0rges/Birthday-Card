cac63044355da3e7a3ce2a44b73d8dbe9e73df9a
Ge0rges <ge0rges@ge0rges.com>
2018-10-11 15:05:21 -0400

    Added App Icon. Made flower nicer. Moved drawing to background.

--- Birthday Card/FlowerView.swift
+++ Birthday Card/FlowerView.swift
@@ -47,10 +47,13 @@ class FlowerView: UIView {
 
 		
 		// Draw the flower
-		drawFlower()
+		let flowerFrame = self.frame
+		DispatchQueue.global(qos: .userInteractive).async {
+			self.drawFlower(frame: flowerFrame)
+		}
 	}
 	
-	func drawFlower() {
+	func drawFlower(frame: CGRect) {
 		// Draw a flower (UIBezierPath) using polar coordinates: r = cos (k\theta) + c
 		// We chose k = 9/4; c = 5 see https://en.wikipedia.org/wiki/Rose_(mathematics)
 		// to learn how different parameters affect the shape.
@@ -61,7 +64,7 @@ class FlowerView: UIView {
 		
 		let flowers: Array<Double>! = [Double(100), Double(50)] // Each item represents a flower length
 		for amplitude in flowers { // len(flowers) = # of flowers
-			for theta in stride(from: 0, to: Double.pi * 2 * 100, by: 0.1) {// Wtf shouldn't theta be going from 0 to 3.28
+			for theta in stride(from: 0, to: Double.pi * 2 * 100, by: 0.0001) {// Wtf shouldn't theta be going from 0 to 3.28
 				// Convert polar coordinates to cartesion coordinates
 				let x: Double = amplitude * cos(k*theta) * cos(theta)
 				let y: Double = amplitude * cos(k*theta) * sin(theta)
@@ -69,8 +72,8 @@ class FlowerView: UIView {
 				// Scale the coordinates to the view
 				// newvalue = (max'-min')/(max-min)*(value-max)+max'
 				let scale_factor:Double = flowers.max()! // Should be largest flower so that smaller flowers appear smaller.
-				let scaled_x:Double = Double(self.frame.width)/(scale_factor*2) * (x - scale_factor) + Double(self.frame.width)
-				let scaled_y:Double = Double(self.frame.height)/(scale_factor*2) * (y - scale_factor) + Double(self.frame.height)
+				let scaled_x:Double = Double(frame.width)/(scale_factor*2) * (x - scale_factor) + Double(frame.width)
+				let scaled_y:Double = Double(frame.height)/(scale_factor*2) * (y - scale_factor) + Double(frame.height)
 
 				if theta == 0 {
 					// (100, 0) then (50, 0)
@@ -85,16 +88,16 @@ class FlowerView: UIView {
 		
 		flowerPath.close()
 		
-		print ("Done path.")
-		
 		// Create a mask with the path
-		let flowerShapeLayer = CAShapeLayer()
-		flowerShapeLayer.path = flowerPath.cgPath
-		flowerShapeLayer.strokeColor = self.strokeColor!.cgColor
-		flowerShapeLayer.fillColor = self.fillColor!.cgColor
-		flowerShapeLayer.fillRule = .evenOdd
-		
-		// Add the layer
-		self.layer.addSublayer(flowerShapeLayer)
+		DispatchQueue.main.async {
+			let flowerShapeLayer = CAShapeLayer()
+			flowerShapeLayer.path = flowerPath.cgPath
+			flowerShapeLayer.strokeColor = self.strokeColor!.cgColor
+			flowerShapeLayer.fillColor = self.fillColor!.cgColor
+			flowerShapeLayer.fillRule = .evenOdd
+			
+			// Add the layer
+			self.layer.addSublayer(flowerShapeLayer)
+		}
 	}
 }
--- Birthday Card/ViewController.swift
+++ Birthday Card/ViewController.swift
@@ -14,19 +14,18 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 		
-		// Set the background color
+		// Set the background color 
 		self.view.backgroundColor = #colorLiteral(red: 0.7970689535, green: 1, blue: 0, alpha: 1)
 		
-		DispatchQueue.main.async {
-			let frameWidth:CGFloat  = self.view.frame.width
-			let frameHeight:CGFloat = self.view.frame.height
-			let flowerDimension:CGFloat = frameWidth * 0.6
-
-			let flowerFrame:CGRect = CGRect(x: frameWidth/2 - flowerDimension/2, y: frameHeight/10, width: flowerDimension, height: flowerDimension)
-			let fillPatternColor: UIColor = UIColor.init(patternImage: UIImage(named: "flowerPattern")!)
-			let flowerView: FlowerView = FlowerView(frame: flowerFrame, strokeColor: #colorLiteral(red: 0.9725490196, green: 0.8274509804, blue: 0.2901960784, alpha: 1), fillColor: fillPatternColor)
-			self.view.addSubview(flowerView)
-		}
+		// Create the flower view
+		let frameWidth:CGFloat  = self.view.frame.width
+		let frameHeight:CGFloat = self.view.frame.height
+		let flowerDimension:CGFloat = frameWidth * 0.6
+		
+		let flowerFrame:CGRect = CGRect(x: frameWidth/2 - flowerDimension/2, y: frameHeight/10, width: flowerDimension, height: flowerDimension)
+		let fillPatternColor: UIColor = UIColor.init(patternImage: UIImage(named: "flowerPattern")!)
+		let flowerView: FlowerView = FlowerView(frame: flowerFrame, strokeColor: #colorLiteral(red: 0.5333333333, green: 0.4470588235, blue: 0.137254902, alpha: 1), fillColor: fillPatternColor)
+		self.view.addSubview(flowerView)
 	}
 }
 
