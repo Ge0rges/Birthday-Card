d83b3ed509a6b862defb981cad78b7579f9178f9
Ge0rges <ge0rges@ge0rges.com>
2018-10-11 14:34:03 -0400

    Fixed flower scaling

--- Birthday Card/FlowerView.swift
+++ Birthday Card/FlowerView.swift
@@ -30,13 +30,10 @@ class FlowerView: UIView {
 	func setup() {
 		
 		// Frame & Layer
-		self.frame = CGRect(x: self.frame.origin.x, y: self.frame.origin.y, width: self.frame.width, height: self.frame.width) // Force Square
-		self.clipsToBounds = true
-		
+		self.frame = CGRect(x: self.frame.origin.x, y: self.frame.origin.y,
+							width: self.frame.width, height: self.frame.width) // Force Square
+
 		// Colors
-		self.backgroundColor = UIColor.blue
-		self.layer.backgroundColor = UIColor.clear.cgColor
-		
 		if self.strokeColor == nil {
 			self.strokeColor = #colorLiteral(red: 0.9803921569, green: 0.7843137255, blue: 0.2901960784, alpha: 1)
 		}
@@ -45,54 +42,61 @@ class FlowerView: UIView {
 			self.fillColor = #colorLiteral(red: 0.9803921569, green: 0.7843137255, blue: 0.2901960784, alpha: 1)
 		}
 		
+		self.backgroundColor = UIColor.clear
+		self.layer.backgroundColor = UIColor.clear.cgColor
+
+		
 		// Draw the flower
 		drawFlower()
 	}
 	
 	func drawFlower() {
 		// Draw a flower (UIBezierPath) using polar coordinates: r = cos (k\theta) + c
-		// We chose k = 7/2; c = 5 see https://en.wikipedia.org/wiki/Rose_(mathematics)
+		// We chose k = 9/4; c = 5 see https://en.wikipedia.org/wiki/Rose_(mathematics)
 		// to learn how different parameters affect the shape.
 		let flowerPath = UIBezierPath()
 		flowerPath.lineWidth = 1.0
 		
-		let k: Double = 9/4 // n/d. If odd k = petals , if even k = petals/2.
+		let k: Double = 9/4 // n/d. If odd k = petals , if even k = petals/2. Check 6
 		
-		for amplitude in [Double(100), Double(50)] {// length of each petal (2) flowers
-			for theta in stride(from: 0, to: Double.pi * 2 * 100, by: 0.001) {// Wtf shouldn't theta be going from 0 to 3.28
+		let flowers: Array<Double>! = [Double(100), Double(50)] // Each item represents a flower length
+		for amplitude in flowers { // len(flowers) = # of flowers
+			for theta in stride(from: 0, to: Double.pi * 2 * 100, by: 0.1) {// Wtf shouldn't theta be going from 0 to 3.28
 				// Convert polar coordinates to cartesion coordinates
 				let x: Double = amplitude * cos(k*theta) * cos(theta)
 				let y: Double = amplitude * cos(k*theta) * sin(theta)
 				
 				// Scale the coordinates to the view
-				let scaled_x:Double = (x + amplitude) / (amplitude*2) * Double(self.frame.width)
-				let scaled_y:Double = (y + amplitude) / (amplitude*2) * Double(self.frame.height)
+				// newvalue = (max'-min')/(max-min)*(value-max)+max'
+				let scale_factor:Double = flowers.max()! // Should be largest flower so that smaller flowers appear smaller.
+				let scaled_x:Double = Double(self.frame.width)/(scale_factor*2) * (x - scale_factor) + Double(self.frame.width)
+				let scaled_y:Double = Double(self.frame.height)/(scale_factor*2) * (y - scale_factor) + Double(self.frame.height)
 
-				
 				if theta == 0 {
-					flowerPath.move(to: CGPoint(x: scaled_x, y: scaled_y))
+					// (100, 0) then (50, 0)
+					flowerPath.move(to: CGPoint(x:scaled_x, y: scaled_y))
+					// flowerPath.move(to: CGPoint(x: x, y: y))
 				} else {
-					flowerPath.addLine(to: CGPoint(x: scaled_x, y: scaled_y))
+					// flowerPath.addLine(to: CGPoint(x: x, y:y))
+					flowerPath.addLine(to: CGPoint(x:scaled_x, y: scaled_y))
 				}
 			}
 		}
 		
+		flowerPath.close()
+		
+		print ("Done path.")
+		
 		// Create a mask with the path
-		let flowerLayer = CAShapeLayer()
-		flowerLayer.path = flowerPath.cgPath
-		flowerLayer.strokeColor = self.strokeColor!.cgColor
-		flowerLayer.fillColor = self.fillColor!.cgColor
-		flowerLayer.fillRule = .evenOdd
-		flowerLayer.backgroundColor = UIColor.clear.cgColor
+		let flowerShapeLayer = CAShapeLayer()
+		flowerShapeLayer.path = flowerPath.cgPath
+		flowerShapeLayer.strokeColor = self.strokeColor!.cgColor
+		flowerShapeLayer.fillColor = self.fillColor!.cgColor
+		flowerShapeLayer.fillRule = .evenOdd
+		flowerShapeLayer.backgroundColor = UIColor.blue.cgColor
 		
 		// Add the layer
-		self.layer.addSublayer(flowerLayer)
-//
-//		// Animate the drawing
-//		let animation = CABasicAnimation(keyPath: "strokeEnd")
-//		animation.fromValue = 0.0
-//		animation.toValue = 1.0
-//		animation.duration = 20.0
-//		flowerLayer.add(animation, forKey: "drawLineAnimation")
+		self.layer.backgroundColor = UIColor.red.cgColor
+		self.layer.addSublayer(flowerShapeLayer)
 	}
 }
